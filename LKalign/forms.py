7315b2e5ed0a16d4c4027a9a1e378c6298506566
alex123012 <makhonin.a.ru@gmail.com>
2021-04-28 17:27:29 +0300

    add similarity count

--- LKalign/forms.py
+++ LKalign/forms.py
@@ -5,3 +5,5 @@ class FileFieldForm(forms.Form):
     title = forms.CharField(max_length=50, label='Title for alignments divided by spaces', required=False)
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}),
                                  label='Upload file(s)')
+    similarity = forms.CharField(max_length=128, label='Proteins to similarity count in alignments divided by spaces',
+                                 required=False)
--- LKalign/views.py
+++ LKalign/views.py
@@ -24,14 +24,15 @@ class FileFieldView(FormView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)  # Getting form for template
         files = request.FILES.getlist('file_field')
+
         resp = request.POST
         if form.is_valid():
             matrix = os.path.join('LKalign', 'static', 'matrix.txt')
             names = resp['title'].split()
-            for file, name in zip(files, names):
+            proteins = request.POST['similarity'].split()
+            for file, name, sim_search in zip(files, names, proteins):
                 try:
 
-                    # x = 'Only_blast/apl_lynx2_blast.fasta'
                     filename = os.path.join(request.COOKIES['sessionid'], f'{name}.aln')
                     filename = os.path.join('LKalign', 'static', 'media', filename)
                     with open(filename, 'w') as f:
@@ -41,13 +42,17 @@ class FileFieldView(FormView):
                         f.write(tmp)
 
                     cmd = ClustalwCommandline('clustalw', infile=filename, gapext=0.5, align=True,
-                                              gapopen=10, matrix=matrix,
-                                              pwmatrix=matrix,
-                                              type='PROTEIN', outfile=filename, quiet=True)
+                                              gapopen=10, matrix=matrix, pwmatrix=matrix,
+                                              type='PROTEIN', outfile=filename, quiet=True,
+                                              terminalgap=10
+                                              )
                     cmd()
                     alignment = AlignIO.read(filename, "clustal")
+                    reference = [(str(i.seq), i.id) for i in alignment if sim_search in i.id][0]
                     max_len = max([len(i.id) + 1 for i in alignment])
-                    alignment = ''.join([str(i.id) + ' ' * (max_len - len(i.id)) + str(i.seq) + '\n' for i in alignment])
+                    alignment = ''.join([str(i.id) + ' ' * (
+                            max_len - len(i.id)) + str(i.seq) + similarity(
+                        i.seq, reference[0], i.id, reference[1]) + '\n' for i in alignment])
                 except Exception:
                     return redirect(request.path)
 
@@ -78,3 +83,19 @@ def check_status(request):
         request.session['filenames'] = []
 
     return request.session['filenames']
+
+
+def similarity(x, y, inid, reference):
+    d = {'G': 0, 'A': 0, 'V': 0, 'L': 0, 'I': 0, 'P': 0, 'M': 0, 'F': 0, 'W': 0,
+         'S': 1, 'T': 1, 'C': 1, 'Y': 1, 'N': 1, 'Q': 1,
+         'D': 2, 'E': 2,
+         'K': 3, 'R': 3, 'H': 3, '-': 4}
+    res = 0
+    if inid in reference:
+        return f'  {len(y)}/{len(x)}'
+    for i, j in enumerate(x):
+        # if j == '-' or y[i] == '-':
+        #     continue
+        if d[j] == d[y[i]]:
+            res += 1
+    return f'  {res}/{len(x)}'
