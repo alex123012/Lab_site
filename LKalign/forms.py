469c443ea3ba098fa358adccdd04adda416bffe2
Alex Makh <alexmakh@bio-neuro.local.nmr.ru>
2021-04-28 17:32:10 +0300

    refactor similarity

--- LKalign/forms.py
+++ LKalign/forms.py
@@ -3,7 +3,8 @@ from django import forms
 
 class FileFieldForm(forms.Form):
     title = forms.CharField(max_length=50, label='Title for alignments divided by spaces', required=False)
-    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}),
-                                 label='Upload file(s)')
     similarity = forms.CharField(max_length=128, label='Proteins to similarity count in alignments divided by spaces',
                                  required=False)
+    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}),
+                                 label='Upload file(s)')
+
